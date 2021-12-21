b902f108cebd3bf1a49ce7c7f5679bc8d59a4882
Sam Macbeth <smacbeth@duckduckgo.com>
2021-12-21 15:54:57 +0000

    Add proxy server option.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
 
+const proxy = process.env.PROXY_SERVER ? { server: process.env.PROXY_SERVER } : null
+
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -10,7 +12,10 @@ const config: PlaywrightTestConfig = {
   projects: [
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        ...devices['Desktop Safari'],
+        proxy,
+      },
     },
   ],
 };
