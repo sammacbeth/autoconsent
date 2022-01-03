7ba00e491c6f64d63074b7ca70b4168dd0109009
Samuel Macbeth <smacbeth@duckduckgo.com>
2022-01-03 13:50:44 -0500

    Add more tests

--- tests/fundingchoices.spec.ts
+++ tests/fundingchoices.spec.ts
@@ -5,5 +5,6 @@ generateCMPTests(
         'https://www.schulferien.org/',
         'https://www.bbc.com/',
         'https://www.accuweather.com/',
+        'https://www.lifehack.org/',
     ].map(site => [site, 'funding-choices'])
 );
